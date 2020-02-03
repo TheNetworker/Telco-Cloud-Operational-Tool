888baa8b4446fb4afc23a09650eda19d00674799
MubarakElrayah <mubarakelrayah@icloud.com>
2020-02-03 06:12:14 +0300

    Updating feature (15) cloud + 15 OS section.

--- README.md
+++ README.md
@@ -8,9 +8,43 @@ Requirements:
 * PyInquirer Package.
 * Paramiko
 
+List of features:
+
+- Cloud
+    - Openstack services status
+    - Ceph Storage status
+    - Controllers status
+    - Computes status
+    - VM's Status
+    - Director Health-check + status
+    - Check for Errors per OpenStack Service.
+    - Show Servers List.
+    - Show Projects List.
+    - Show Users List.
+    - Show Networks List.
+    - Show Volumes List.
+    - Show Images List.
+    - Check OpenStack Host Aggregate (HA).
+    - Check Baremetal List and status.
+- OS
+    - Display the running and active real-time processes.
+    - NUMA Status.
+    - Number of running VM
+    - Number of virtual interfaces (VIF)
+    - Disk status.
+    - Memory status.
+    - Check SRIOV status.
+    - Check Capabilities of OS
+    - Check for Hardware Errors.
+    - Check Hardware Information.
+    - Generate sosreport.
+    - Check Service status.
+    - Check Neighbors reachability.
+    - Check grub parameter
+    - Run Command on host.
 
 Reference Link:
 
 * PyInquirer: https://pypi.org/project/PyInquirer/
 * Ansible Doc: https://docs.ansible.com/
-* Paramiko Doc: 
+* Paramiko Doc:
--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-#!/usr/local/bin/python3.7
-
 from __future__ import print_function, unicode_literals
 from PyInquirer import style_from_dict, Token, prompt, Separator
 from pprint import pprint
+import json
+import emoji
 
 
 style = style_from_dict({
@@ -16,56 +16,86 @@ style = style_from_dict({
 })
 
 
-questions = [
-    {
-        'type': 'checkbox',
-        'message': 'Usage:',
-        'name': 'action',
-        'choices': [
-            Separator('= Health Check ='),
-            {
-                'name': 'Undercloud'
-            },
-            {
-                'name': 'Overcloud'
-            },
-            {
-                'name': 'Ceph Storage'
-            },
-            Separator('= Collect Logs ='),
-            {
-                'name': 'sosreport',
-                'disabled': 'Administrator Only'
-                # 'checked': True
-            },
-            Separator('= Nodes Information ='),
-            {
-                'name': 'Baremetal Node list'
-            },
-            {
-                'name': 'Overcloud Node list'
-            },
-            {
-                'name': 'List of VM\'s per Node'
-            },
-            {
-                'name': 'List of VM\'s with their respective NUMA'
-            },
-            {
-                'name': 'List of VM\'s with their vCPU'
-            },
-            {
-                'name': 'List of VM\'s with their vNIC'
-            },
-            {
-                'name': 'List of VM\'s with their RAMs info'
-            }
+def questionsList():
+    questions = [
+        {
+            'type': 'input',
+            'name': 'host',
+            'message': emoji.emojize('Insert ansible device group name  :busts_in_silhouette: :'),
 
-        ],
-        'validate': lambda answer: 'You must choose at least one topping.' \
+        },
+        {
+            'type': 'input',
+            'name': 'inventory',
+            'message': emoji.emojize('Inventory file Path  :open_file_folder: ?'),
+
+        },
+        {
+            'type': 'list',
+            'name': 'tasks',
+            'message': 'Choose your Task',
+            'choices': [
+                "System",
+                "Cloud \'OpenStack\'",
+                "Storage",
+                "Network"
+            ]
+        },
+        {
+            'type': 'checkbox',
+            'message': 'Selection:',
+            'name': 'action',
+            'choices': [
+                Separator('= Health Check ='),
+                {
+                    'name': 'Undercloud'
+                },
+                {
+                    'name': 'Overcloud'
+                },
+                {
+                    'name': 'Ceph Storage'
+                },
+                Separator('= Collect Logs ='),
+                {
+                    'name': 'sosreport',
+                    # 'disabled': 'Administrator Only'
+                    # 'checked': True
+                },
+                Separator('= Nodes Information ='),
+                {
+                    'name': 'Baremetal Node list'
+                },
+                {
+                    'name': 'Overcloud Node list'
+                },
+                {
+                    'name': 'List of VM\'s per Node'
+                },
+                {
+                    'name': 'List of VM\'s with their respective NUMA'
+                },
+                {
+                    'name': 'List of VM\'s with their vCPU'
+                },
+                {
+                    'name': 'List of VM\'s with their vNIC'
+                },
+                {
+                    'name': 'List of VM\'s with their RAMs info'
+                }
+
+            ],
+            'validate': lambda answer: 'You must choose at least one topping.' \
             if len(answer) == 0 else True
-    }
-]
+        }
+    ]
+    return questions
 
-answers = prompt(questions, style=style)
+
+def healthCheck():
+    print('run ansible health check ....')
+
+
+answers = prompt(questionsList(), style=style)
 pprint(answers)
